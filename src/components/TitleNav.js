b5863a69828f44a1ecb2f1f3b9bb379ab5c94f9e
beyond_th0ught <yadavkhushboo7653@gmail.com>
2022-11-14 19:28:25 +0530

    created NavTop NavBottom and TitleNav

---
+++ src/components/TitleNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TitleNav() {
+  return (
+    <div className='bg-blue-400 py-7'>
+        <h1 className=' font-[poppins] hover:cursor-pointer font-medium text-sm '>UI/UX  Refer & Earn</h1>
+    </div>
+  )
+}
+
+export default TitleNav
